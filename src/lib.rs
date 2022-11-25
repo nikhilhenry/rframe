6035e86d8c59dfc07862cea5ca6641fa1711b042
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-11-25 19:44:13 +0530

    remove match expressions for error

--- src/lib.rs
+++ src/lib.rs
@@ -27,23 +27,9 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // read user image
-    let screenshot = match image::open(config.file_path) {
-        Ok(file) => file,
-        Err(e) => return Err(Box::new(e)),
-    };
-    // generate correct bezel
-    let bezel = match frame::Bezel::build(String::from("iPhone13")) {
-        Ok(file) => file,
-        Err(e) => return Err(Into::into(e)),
-    };
-
-    let img = match frame::render(screenshot, bezel.frame) {
-        Ok(img) => img,
-        Err(e) => return Err(e),
-    };
-    if let Err(e) = img.save(config.output_path) {
-        return Err(Box::new(e));
-    }
+    let screenshot = image::open(config.file_path)?;
+    let bezel = frame::Bezel::build(String::from("iPhone13"))?;
+    let img = frame::render(screenshot, bezel.frame)?;
+    img.save(config.output_path)?;
     return Ok(());
 }
